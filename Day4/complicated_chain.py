e31fb4d3d696e9ce06a2ed8cc0ac88ff49c21459
drdeford <ddeford@math.dartmouth.edu>
2019-06-26 16:53:45 -0400

    settled on 50k

--- Day4/complicated_chain.py
+++ Day4/complicated_chain.py
@@ -186,10 +186,10 @@ def annealing_cut_accept2(partition):
 
     t = partition["step_num"]
 
-    if t < 100000:
+    if t < 10000:
         beta = 0
-    elif t < 400000:
-        beta = (t - 100000) / 100000  # was 50000)/50000
+    elif t < 40000:
+        beta = (t - 10000) / 10000  # was 50000)/50000
     else:
         beta = 3
 
@@ -283,7 +283,7 @@ for exp_num in [40, 20, 1]:  # range(22,31):
         #    Validator([single_flip_contiguous, popbound]),
         #    accept=annealing_cut_accept2,  # aca,#cut_accept,#always_accept,#
         #    initial_state=gp,
-        #    total_steps=500000,
+        #    total_steps=50000,
         #)
 
         pos_dict = {n: n for n in graph.nodes()}
